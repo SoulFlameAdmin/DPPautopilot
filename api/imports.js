024b09ca09c883c76fc67b1921b722c567c84f08
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-19 03:16:49 +0300

    security: enforce R05 limits on imports API

--- api/imports.js
+++ api/imports.js
@@ -2,6 +2,7 @@
 
 const { mapDatabaseError: mapSharedDatabaseError } = require('./_errors.js');
 const { parseBody, bodyErrorResponse } = require('./_request.js');
+const { enforceRateLimit, rateLimitBody } = require('./_rate_limit.js');
 
 function send(res,status,body){
   res.statusCode=status;
@@ -70,6 +71,8 @@ async function rpc(name,payload,authorization,env=process.env,fetchImpl=fetch){
 }
 
 async function handler(req,res){
+  const rateLimit=enforceRateLimit(req,res,'imports');
+  if(!rateLimit.allowed) return send(res,429,rateLimitBody());
   const authorization=bearer(req);
   if(!authorization){
     return send(res,401,{error:{code:'AUTH_REQUIRED',message:'Bearer authentication is required.'}});
