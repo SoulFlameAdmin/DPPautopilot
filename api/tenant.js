3492c2bad71ddcfb988049ffea27debbcced725c
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-19 09:05:34 +0300

    security: rate limit tenant context API

--- api/tenant.js
+++ api/tenant.js
@@ -2,6 +2,7 @@
 
 const { mapDatabaseError: mapSharedDatabaseError } = require('./_errors.js');
 const { parseBody, bodyErrorResponse } = require('./_request.js');
+const { enforceRateLimit, rateLimitBody } = require('./_rate_limit.js');
 const { startRequestObservability } = require('./_observability.js');
 
 function send(res,status,body){
@@ -62,6 +63,8 @@ async function rpc(name,payload,authorization,env=process.env,fetchImpl=fetch){
 
 async function handler(req,res){
   startRequestObservability(req,res,'tenant');
+  const rateLimit=enforceRateLimit(req,res,'tenant');
+  if(!rateLimit.allowed) return send(res,429,rateLimitBody());
 
   const authorization=bearer(req);
   if(!authorization){
